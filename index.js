477d684833a756edc54776e702785dd5e231f520
Ashish Ingle <ingle.ashish@gmail.com>
2017-11-10 17:21:51 +0530

    refactor in sperates files

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
+require('./services/authService');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.get('/', (req, res)=>{
-//   res.send({'name': 'Ashish'});
-// });
+authRoutes(app);
 
-
-
-
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log(`user accessToken is : ${accessToken}`);
-    console.log(profile)
-  })
-);
-
-app.get('/auth/google/', passport.authenticate('google',{
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT, ()=>{
   console.log(`Started server on port ${PORT}`);
