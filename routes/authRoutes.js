d5f3a087c86bf51169dafa1d3e43afaed1a0a076
Ashish Ingle <ingle.ashish@gmail.com>
2017-11-13 17:56:49 +0530

    added client react app and logout functionality to server

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,10 @@ module.exports = (app) => {
   app.get('/api/user', (req, res) => {
     console.log(req.user)
     res.send(req.user);
-  })
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
